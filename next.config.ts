1d7b5416451c50769fcdd0f2334124efb1c97754
Muhammadazim <shuhrataliyevm@gmail.com>
2025-05-04 13:16:38 +0500

    n

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
 export default nextConfig;
